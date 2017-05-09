40c8fc0358202390f3718903e12a2f353f5f5886
Victor Fernandes <victor.fernandes@webedia-group.com>
2017-05-09 17:15:45 -0300

    es6 class

--- main.js
+++ main.js
@@ -1,46 +1,60 @@
-"use strict";
+class App {
 
-var entry = "1-HOME-SWEET-HOME";
+	constructor(){
+		this.charMap = {
+			0: 0,
+			1: 1,
+			"-": "-",
+			A: 2,
+			B: 2,
+			C: 2,
+			D: 3,
+			E: 3,
+			F: 3,
+			G: 4,
+			H: 4,
+			I: 4,
+			J: 5,
+			K: 5,
+			L: 5,
+			M: 6,
+			N: 6,
+			O: 6,
+			P: 7,
+			Q: 7,
+			R: 7,
+			S: 7,
+			T: 8,
+			U: 8,
+			V: 8,
+			W: 9,
+			X: 9,
+			Y: 9,
+			Z: 9
+		}
+	}
 
-entry = entry.split("");
+	read(){
+		const lineReader = require('readline').createInterface({
+		  input: require('fs').createReadStream('teste')
+		});
 
-const charMap = {
-	0: 0,
-	1: 1,
-	"-": "-",
-	A: 2,
-	B: 2,
-	C: 2,
-	D: 3,
-	E: 3,
-	F: 3,
-	G: 4,
-	H: 4,
-	I: 4,
-	J: 5,
-	K: 5,
-	L: 5,
-	M: 6,
-	N: 6,
-	O: 6,
-	P: 7,
-	Q: 7,
-	R: 7,
-	S: 7,
-	T: 8,
-	U: 8,
-	V: 8,
-	W: 9,
-	X: 9,
-	Y: 9,
-	Z: 9
-};
+		lineReader.on('line', (line) => {
+			this.processLine.call(this, line);
+		});
+	}
 
+	processLine(line) {
+		line = line.split("");
+		var exit = line.map(this.prepareExit.bind(this));
+		console.log(exit.join(""));
+	}
 
-var exit = entry.map(prepareExit);
-
-function prepareExit (char) {
-	return charMap[char];
+	prepareExit (char){
+		return this.charMap[char];
+	}
 }
 
-console.log(exit.join(""));
+const app = new App();
+
+app.read();
