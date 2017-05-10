4f99794747e075a30146c6895d33d4caf4cb0310
Victor Fernandes <victor.fernandes@webedia-group.com>
2017-05-10 14:10:00 -0300

    adding comments and translating error messages

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/*
+	Requirements: Node.js
+	run $ node main.js path/to/file
+*/
+
 class App {
 
 	constructor(){
@@ -35,35 +40,39 @@ class App {
 	}
 
 	read(file){
-		const fs = require('fs');
+		const fs = require('fs'); //require the file system module
 
+		//check if file paramather is not empty
 		if(!file){
-			console.log('Erro: O parâmentro de arquivo de entrada não pode ser vazio. Formato aceito: $ node main.js /arquivo/de/entrada');
+			console.log('Error: The input file parameter must not be empty. Example: $ node main.js /arquivo/de/entrada');
 			return;
 		}
+		//check if file exists
 		if(!fs.existsSync(file)){
-			console.log(`Erro: O Arquivo ${file} não existe.`);
+			console.log(`Error: The ${file} file does not exist.`);
 			return;
 		}
+		//require readline module
 		const lineReader = require('readline').createInterface({
 		  input: fs.createReadStream(file)
 		});
-
+		//read line by line until end of file
 		lineReader.on('line', (line) => {
 			this.processLine.call(this, line);
 		});
 	}
 
+	//string manipulations
 	processLine(line) {
 		line = line.split("");
 		let exit = line.map(this.prepareExit.bind(this));
 		console.log(exit.join(""));
 	}
-
+	//read the map and find the correspondent value
 	prepareExit (char){
 		return this.charMap[char];
 	}
 }
 
 const app = new App();
-app.read(process.argv[2]);
+app.read(process.argv[2]); //input file will be the third parameter the user types on console
